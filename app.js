ae174695f403d2764ea61a1eae266c642638f4b8
JB <lustereaper@DasPerpetualBinary>
2023-04-27 18:07:41 -0600

    aportacion lc

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// Escribe tu código aquí.
+function bigWords(str, arr) {
+    let result = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length > str.length) {
+        result.push(arr[i]);
+      }
+    }
+  
+    return result;
+  }
+  function printArray(arr) {
+    const ul = document.getElementById("myList");
+  
+    for (let i = 0; i < arr.length; i++) {
+      const li = document.createElement("li");
+      li.innerText = arr[i];
+      ul.appendChild(li);
+    }
+  }
+
+
+const button = document.getElementById("button");
+button.addEventListener("click", handleClick);
+
+
+function handleClick() {
+
+  const input = document.getElementById("input").value;
+  const words = ["insecto", "bootcamp", "mangos", "reptil", "mosca", "escritorio", "computadora", "libro", "manzana", "perro"];
+
+
+  const bigWordsArr = bigWords(input, words);
+  printArray(bigWordsArr);
+}
